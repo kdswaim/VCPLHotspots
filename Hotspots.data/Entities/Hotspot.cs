3e595b24787083f11f48a449ba67e110d99662a1
Kristen Swaim <106117219+kdswaim@users.noreply.github.com>
2023-07-21 15:19:34 -0400

    Fleshed out entities

--- Hotspots.data/Entities/Hotspot.cs
+++ Hotspots.data/Entities/Hotspot.cs
@@ -7,9 +7,31 @@ namespace Hotspots.data.Entities
 {
     public class Hotspot
     {
+        [Required]
         public int HotspotId {get; set;}
+        [Required]
         public string HotspotName {get; set;}
+        [Required]
         public string Password {get; set;}
-        
+        [Required]
+        public string SerialNumber {get; set;}
+        [Required]
+        public string AdminPassword {get; set;}
+        [Required]
+        public string MobileNumber {get; set;}
+        [Required]
+        public string SIM {get; set;}
+        [Required]
+        public string IMEI {get; set;}
+        [Required]
+        public string DeviceModelName {get; set;}
+        [Required]
+        public string SSID {get; set;}
+        [Required]
+        public string OriginalSSID {get; set;}
+        [Required]
+        public string OriginalPassword {get; set;}
+        [Required]
+        public string Notes {get; set}
     }
 }
