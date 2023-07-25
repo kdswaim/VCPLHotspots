11b9f50b15d92a98b057b0d3a5cde8ffe993d042
Kkilker <kkilker@vigo.lib.in.us>
2023-07-25 12:31:37 -0400

    "completed ef migrations"

--- Hotspots.data/Entities/TroubleshootingGuide.cs
+++ Hotspots.data/Entities/TroubleshootingGuide.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Hotspots.data.Entities
 {
     public class TroubleshootingGuide
     {
+        [Key]
         public int GuideId {get; set;}
         public int HotspotId {get; set;}
         public string GuideTitle {get; set;}
