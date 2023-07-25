8041902b39170e3ca4bcc5d422116614f0feb988
Kkilker <kkilker@vigo.lib.in.us>
2023-07-25 11:57:35 -0400

    "committing changes"

--- Hotspots.data/Entities/TroubleshootingGuide.cs
+++ Hotspots.data/Entities/TroubleshootingGuide.cs
@@ -7,6 +7,11 @@ namespace Hotspots.data.Entities
 {
     public class TroubleshootingGuide
     {
-        
+        public int GuideId {get; set;}
+        public int HotspotId {get; set;}
+        public string GuideTitle {get; set;}
+        public string GuideContent {get; set;}
+
+        public Hotspot Hotspot {get; set;}
     }
 }
---
+++ Hotspots.data/HotspotContext/HotspotDBContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hotspots.data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hotspots.data.HotspotContext
+{
+    public class HotspotDBContext : IdentityDbContext<User>
+    {
+        public HotspotDBContext(DbContextOptions options):base(options){}
+        
+        public DbSet<Hotspot> Hotspots { get; set; }
+
+        public DbSet <TroubleshootingGuide> TroubleshootingGuide {get; set;}
+        }
+    }
